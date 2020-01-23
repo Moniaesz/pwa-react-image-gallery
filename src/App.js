0876efe7393120ea9bcc8f3e43032270cee44fc5
Monika <molinskamonika+github@gmail.com>
2020-01-23 16:58:40 +0100

    create app header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>React Gallery</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import './Header.css'
+
+function Header () {
+  return (
+    <header className='app-header'>
+      <img src={logo} alt='app logo' className='app-logo' />
+      <h1>React PHOTO-GALLERY</h1>
+    </header>
+  )
+}
+
+export default Header;
